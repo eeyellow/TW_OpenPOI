a478267e60840cb1a34f239ede67e36f045a8248
eeyellow <eeyellow@gmail.com>
2016-08-29 13:02:37 +0800

    [init] Branch: GetData

---
+++ GetData/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using System.IO;
+
+namespace GetData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
