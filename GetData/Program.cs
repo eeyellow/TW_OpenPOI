0975f3cdbb776fca9a39d3ac9593a404164ab753
eeyellow <eeyellow@gmail.com>
2016-08-29 15:04:59 +0800

    [add] Database.cs
    A class that operating PostgreSQL Database

--- GetData/Program.cs
+++ GetData/Program.cs
@@ -12,7 +12,21 @@ namespace GetData
         static void Main(string[] args)
         {
             var listOfHospital = Utility.GetCSV(ConfigurationManager.AppSettings["URL_Hospital"]);
-
+            
+            Database db = new Database(ConfigurationManager.ConnectionStrings["DB_TWPOI"].ToString());
+            db.setCommand("SELECT id FROM medical_type WHERE name='醫院'");
+            int type = (int)db.ExecuteScalar();
+            for(int i = 1; i < listOfHospital.Count; i++)
+            {
+                SortedList<string, object> param = new SortedList<string, object>();
+                param.Add("@name", listOfHospital[i][0]);
+                param.Add("@phone", listOfHospital[i][1]);
+                param.Add("@address", listOfHospital[i][2]);
+                param.Add("@type", type);
+                db.setCommand("INSERT INTO medical(name,phone,address,type) VALUES(@name,@phone,@address,@type)", param);
+                db.ExecuteNonQuery();
+            }
+            
         }
     }
 }
